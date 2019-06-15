645dcdbed868181ee612ba792e0488a42a143db9
charles bolton <charboltron@gmail.com>
2019-06-14 18:31:09 -0700

    fixed duplicate directories/files issue

--- saw_waves/chords/renamer.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-current_dir = '/Users/charlesbolton/Desktop/510_Music/sound_project/ear_trainer/saw_waves/chords/'
-
-for file_name in os.listdir(current_dir):
-    if '.wav' in file_name:
-        file_name_replace = file_name.replace(' ', '_')
-        shutil.move(file_name, current_dir+file_name_replace)
